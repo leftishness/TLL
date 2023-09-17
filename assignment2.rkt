f82a759906db10016530c9ba25836380fccf3c92
leftishness <ctyler.clark@gmail.com>
2023-09-17 15:40:29 -0400

    Update assignment2.rkt

--- assignment2.rkt
+++ assignment2.rkt
@@ -2,13 +2,16 @@
 (require malt)
 
 ; problem 2
-(define prod-1 (lambda (t) (prodded t (sub1 (tlen t)) 0.0)))
+(define prod-1
+  (lambda (t)
+    (prodded t (sub1 (tlen t)) 1.0)))
 
 (define prodded
   (lambda (t i a)
     (cond
-      [(zero? i)
-       (+ (tref t i) a)
-       (else (prodded t (sub1 i) (+ (tref t i) a)))])))
+      ((zero? i) a)
+      (else
+       (prodded t (sub1 i) (* (tref t i) a))))))
 
 (prod-1 (tensor 1.0 2.0 3.0))
+(prod-1 (tensor 2.0 3.0 6.0 2.0))
