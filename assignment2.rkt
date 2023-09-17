cfc81b6c00dadd74b265389aad3e6286f58b106e
leftishness <ctyler.clark@gmail.com>
2023-09-17 16:06:16 -0400

    Update assignment2.rkt

--- assignment2.rkt
+++ assignment2.rkt
@@ -15,3 +15,24 @@
 
 (prod-1 (tensor 1.0 2.0 3.0))
 (prod-1 (tensor 2.0 3.0 6.0 2.0))
+
+; problem 5
+(define new-line-xs
+  (tensor -2.0 -1.0 0.0 1.0 2.0))
+ 
+(define new-line-ys
+  (tensor 4.97 3.13 0.99 -1.0 -3.11))
+
+(define f
+  (lambda (m x b)
+      (+ (* m x) b)))
+
+(define revise
+  (lambda (f revs theta)
+    (cond
+      ((zero? revs) theta)
+      (else
+       (revise f (sub1 revs) (f theta))))))
+
+(define theta
+  (list 0.0 0.0))
